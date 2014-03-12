6af96c85983c4ff4293a1cf4064c2dd861844cbb
Anita Bharambe <anita.bharambe@icicletech.com>
2014-03-12 10:46:47 +0530

    change the name of the file of the javascript

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,12 +3,12 @@ class LikesController < ApplicationController
 def create
   @like = Like.create(params[:like])
     @post = @like.post
-    render :_tog_like_post
+    render :tog_like
 end
 
 def destroy
   like = Like.find(params[:id]).destroy
     @post = like.post
-    render :_tog_like_post
+    render :tog_like
 end
 end
