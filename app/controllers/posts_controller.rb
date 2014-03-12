a83af944c6cb840ccfefc5526c9eeed71d92fe80
Anita Bharambe <anita.bharambe@icicletech.com>
2014-03-12 10:21:29 +0530

    added the user name and change the query to find the user name

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,12 +14,7 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
-    @user= User.find_by_sql("SELECT users.id , users.first_name,users.last_name,posts.id, posts.title FROM `users` LEFT OUTER JOIN posts ON posts.user_id = users.id")
-    @user.each do |name|
-     @first_name = name.first_name
-     @last_name = name.last_name
-    end
-    Rails.logger.debug "--- #{@first_name} #{@last_name} ---"
+    @user = User.find_by_id(@post.user_id)
   end
 
   def new
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-   attr_accessible :title, :content, :post_id, :comment, :commenter,:post_id,:user_id
+   attr_accessible :title, :content, :post_id, :comment, :commenter,:post_id,:user_id,:first_name
    has_many :comments
    validates_presence_of :title ,:message => " Title cannot be blank."
    validates_presence_of :content ,:message => " Content cannot be blank."
